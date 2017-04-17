a088782d41c8b5bc8cc2702f5c0ef782d3ed3f1a
Enoch2k2 <enoch2k2@hotmail.com>
2017-04-17 12:43:29 -0500

    added window.js to base window

---
+++ js/base/window/window.js
@@ -0,0 +1,3 @@
+// todo, add base class for window structure - then branch out to possibly different types of windows?
+// deconstruct the functionality of window - meaning a space used in the canvas.
+// possibly give ability to set background color for the window
